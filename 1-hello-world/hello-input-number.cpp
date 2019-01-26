fb5754aace782322013a13928db3550d5ecabe36
Massimo <massimodemarchi91@gmail.com>
2019-01-26 11:23:06 +0000

    add input number example

---
+++ 1-hello-world/hello-input-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  int a, b;
+
+  std::cout << "Give me the first number: ";
+  std::cin >> a;
+
+  std::cout << "Give me the second number: ";
+  std::cin >> b;
+
+  std::cout << a << " + " << b << " = " << a + b << "\n";
+
+  return 0;
+}
